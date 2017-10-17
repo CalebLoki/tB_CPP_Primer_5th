4a8cef606f4231ee9cb05c1ac102c7e7a1dc0568
FOXMAN <foxman@FOXMANdeMacBook-Pro.local>
2017-10-18 03:08:41 +0800

    Create project

---
+++ cmake-build-debug/CMakeFiles/tB_CPP_Primer_5th.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tB_CPP_Primer_5th.dir/main.cpp.o"
+  "tB_CPP_Primer_5th.pdb"
+  "tB_CPP_Primer_5th"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tB_CPP_Primer_5th.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "Sales_items.h"
+
+using namespace std;
+
+int main() {
+
+    Sales_item v_s_curr, v_s_value;
+
+
+    if(cin >> v_s_curr){
+        while(cin >> v_s_value){
+
+            if(v_s_curr.isbn() == v_s_value.isbn()){
+                v_s_curr += v_s_value;
+            }else{
+                cout << "The total sale of this book are: " << v_s_curr;
+                v_s_curr = v_s_value;
+            }
+        }
+        cout << "The last total sale of this book are: " << v_s_curr;
+    }else{
+        cerr << "No data entry!" << endl;
+        return -1;
+    }
+    return 0;
+}
